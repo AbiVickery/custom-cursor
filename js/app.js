db36879b60b0ac719255f022bf92f4fde8698828
Abi Vickery <vickeryabi@gmail.com>
2019-12-19 13:30:56 -0600

    custom cursor is active

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px");
 })
 
-// document.addEventListener('click', () => {
-//     cursor.classList.add("expand");
-// })
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
